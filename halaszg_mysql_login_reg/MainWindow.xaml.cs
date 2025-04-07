8bb33dff46275a128a1495b52f9720e1974888db
halaszgergely <halaszg@kkszki.hu>
2025-04-07 08:34:37 +0200

    asd

--- halaszg_mysql_login_reg/MainWindow.xaml.cs
+++ halaszg_mysql_login_reg/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace halaszg_mysql_login_reg
     public partial class MainWindow : Window
     {
         //kapcsolati string
-        MySqlConnection connect = new MySqlConnection("server = localhost;database = asztali_11a;uid = root;password = '';");
+        MySqlConnection connect = new MySqlConnection("server = server.fh2.hu;database = v2labgwj_11a;uid = v2labgwj_11a;password = VGFR2GJjqudMt8Q4SA5j;");
         public MainWindow()
         {
             InitializeComponent();
@@ -34,7 +34,7 @@ namespace halaszg_mysql_login_reg
             //kapcsolat megnyitása
             connect.Open();
             //parancs létrehozása
-            var sql = $"SELECT * FROM user WHERE nev = '{txtUser.Text}' AND jelszo = '{txtPass.Password}'";
+            var sql = $"SELECT * FROM halaszg_user WHERE nev = '{txtUser.Text}' AND jelszo = '{txtPass.Password}'";
             lbDebug.Content = sql;
             MySqlCommand cmd = new MySqlCommand(sql, connect);
             //parancs végrehajtása
@@ -48,13 +48,36 @@ namespace halaszg_mysql_login_reg
             {
                 MessageBox.Show("Sikertelen bejelentkezés");
             }
+            reader.Close();
             connect.Close();
-
         }
 
         private void register_Click(object sender, RoutedEventArgs e)
         {
-            
+            //1. Ha a két jelszó nem egyezik nem enged tovább
+            if (reg_pass.Password != reg_passAgain.Password)
+            {
+                MessageBox.Show("A két jelszó nem egyezik");
+                return;
+            }
+            connect.Open();
+            //2. Ha a felhasználónév már létezik nem enged tovább
+            var reader = new MySqlCommand($"SELECT * FROM halaszg_user WHERE nev = '{reg_user.Text}'", connect).ExecuteReader();
+            if (reader.Read())
+            {
+                MessageBox.Show("Ez a felhasználónév már létezik");
+                return;
+            }
+            else
+            {
+                //3. Ha a felhasználónév és a jelszó is megvan, akkor regisztrál
+                reader.Close();
+                var sql = $"INSERT INTO halaszg_user (nev, jelszo) VALUES ('{reg_user.Text}', '{reg_pass.Password}')";
+                lbDebug.Content = sql;
+                new MySqlCommand(sql, connect).ExecuteNonQuery();
+                MessageBox.Show("Sikeres regisztráció");
+            }
+            connect.Close();
         }
     }
 }
